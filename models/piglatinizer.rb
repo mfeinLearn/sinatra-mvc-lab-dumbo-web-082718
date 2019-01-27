3d4ea08e09044c091e03cef2f1b75313ca265689
github username <github email address>
2019-01-26 22:54:01 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,16 +3,17 @@ class PigLatinizer
   attr_accessor :word
 
   def piglatinize(word)
+
     @word = word.downcase
     shifting = word.split("")
     beginning_letter = shifting.shift
-    # binding.pry
-    if beginning_letter.scan(/\A[^aeiou]/)
+     #binding.pry
+    if beginning_letter.scan(/[aeiou]/).first#(/\A[^aeiou]/)
       vowel
-    elsif beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
+      #binding.pry
+    elsif beginning_letter.scan(/[bcdfghjklmnpqrstvwxyz]/).first
       second_letter = shifting.shift
-      # binding.pry
-      if second_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
+      if second_letter.scan(/[bcdfghjklmnpqrstvwxyz]/).first
       consonant_cluster
       # second_letter = shifting.shift
     # elsif second_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
@@ -30,32 +31,33 @@ class PigLatinizer
   def consonant
     shifting = word.split("")
     beginning_letter = shifting.shift
-    if beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
+    #if beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/).first
       shifting << beginning_letter
       shifting << "a"
       shifting << "y"
       shifting.join
-    else
-      puts "nope please type another word(s)"
-    end
+    # else
+    #   puts "nope please type another word(s)"
+    # end
   end
 
   def consonant_cluster
+
     shifting = word.split("")
-    beginning_letter = shifting.shift
-    if beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
-      second_letter = shifting.shift
-      if second_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
+     beginning_letter = shifting.shift
+    # if beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
+       second_letter = shifting.shift
+    #   if second_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
         shifting << beginning_letter
         shifting << second_letter
         shifting << "a"
         shifting << "y"
         shifting.join
-      else
-        puts "nope please type another word(s)"
-      end
+      # else
+      #   puts "nope please type another word(s)"
+      # end
     end
-  end
+  #end
 
   def vowel
     shifting = word.split("")
