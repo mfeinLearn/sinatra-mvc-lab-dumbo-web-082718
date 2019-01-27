92bc6d3d6197aaa06ceb1eb3aaf0d2ebff48ffe5
github username <github email address>
2019-01-26 23:00:37 -0500

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,67 +7,44 @@ class PigLatinizer
     @word = word.downcase
     shifting = word.split("")
     beginning_letter = shifting.shift
-     #binding.pry
-    if beginning_letter.scan(/[aeiou]/).first#(/\A[^aeiou]/)
+    if beginning_letter.scan(/[aeiou]/).first
       vowel
-      #binding.pry
     elsif beginning_letter.scan(/[bcdfghjklmnpqrstvwxyz]/).first
       second_letter = shifting.shift
       if second_letter.scan(/[bcdfghjklmnpqrstvwxyz]/).first
       consonant_cluster
-      # second_letter = shifting.shift
-    # elsif second_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
-    #   consonant_cluster
       else
         consonant
       end
     end
   end
-  # def initialize(word)
-  #   @word = word.downcase
-  #    # binding.pry
-  # end
 
   def consonant
     shifting = word.split("")
     beginning_letter = shifting.shift
-    #if beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/).first
       shifting << beginning_letter
       shifting << "a"
       shifting << "y"
       shifting.join
-    # else
-    #   puts "nope please type another word(s)"
-    # end
   end
 
   def consonant_cluster
 
     shifting = word.split("")
      beginning_letter = shifting.shift
-    # if beginning_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
        second_letter = shifting.shift
-    #   if second_letter.scan(/\A[^bcdfghjklmnpqrstvwxyz]/)
         shifting << beginning_letter
         shifting << second_letter
         shifting << "a"
         shifting << "y"
         shifting.join
-      # else
-      #   puts "nope please type another word(s)"
-      # end
     end
-  #end
 
   def vowel
     shifting = word.split("")
-    # beginning_letter = shifting.shift
-    # if beginning_letter.scan(/\A[^aeiou]/)
-    # shifting << beginning_letter
     shifting << "a"
     shifting << "y"
     shifting.join
-  # end
   end
 
 end
